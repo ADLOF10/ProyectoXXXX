ef1e0d3ef95c07e45d1da6482a1429c34040ff21
marco12-coder <mar342c@gmail.com>
2025-01-04 01:56:35 -0600

    hjgafs
    hgafgaf

--- app/Http/Controllers/QRScannerController.php
+++ app/Http/Controllers/QRScannerController.php
@@ -8,6 +8,8 @@ use App\Models\Asistencia;
 use PHPUnit\Framework\Attributes\Group;
 use Illuminate\Support\Facades\Auth;
 use Carbon\Carbon;
+Carbon::setLocale('es'); // Opcional, para español
+
 
 class QRScannerController extends Controller
 
@@ -35,15 +37,35 @@ class QRScannerController extends Controller
                     return redirect('/qr-scan')->with('success','No te puedes registrar por segunda vez');
                 }
 
+            
+                
+                $horaActual = Carbon::now('America/Mexico_City')->format('Y-m-d H:i:s'); // Hora actual en formato Y-m-d H:i:s
+                $horaActualCarbon = Carbon::now('America/Mexico_City'); // Hora actual como objeto Carbon en la zona horaria correcta
+                $inicio_de_clase = Carbon::parse($request->hora_inicio_clase, 'America/Mexico_City'); // Inicio de clase como Carbon con zona horaria
+                $asistencia = (int)$request->asistencia; // Tolerancia para asistencia (en minutos)
+                $retardo = (int)$request->retardo; // Tolerancia para retardo (en minutos)
+                
+                // Diferencia en minutos desde el inicio de clase
+                $diferenciaEnMinutos = $horaActualCarbon->diffInMinutes($inicio_de_clase, false);
+                
+                if ($diferenciaEnMinutos >= 0 && $diferenciaEnMinutos <= $asistencia) {
+                    $estado = 'asistencia'; // Dentro de la tolerancia para asistencia
+                } elseif ($diferenciaEnMinutos > $asistencia && $diferenciaEnMinutos <= $retardo) {
+                    $estado = 'retardo'; // Dentro de la tolerancia para retardo
+                } else {
+                    $estado = 'falta'; // Fuera de la tolerancia
+                }
+            
 
-        // Crear la entrada en la base de datos
-        Asistencia::create([
-            'alumno_id' => $userId,
-            'grupo_id' => $request->id_grupo,
-            'fecha' => $request->fecha,
-            'hora_registro' => $request->hora_inicio_clase,
-            'estado' => $request->falta
-        ]);
+
+                    // Crear la entrada en la base de datos
+                    Asistencia::create([
+                        'alumno_id' => $userId,
+                        'grupo_id' => $request->id_grupo,
+                        'fecha' => $request->fecha,
+                        'hora_registro' => $inicio_de_clase,
+                        'estado' => $estado
+                    ]);
 
                 
                 return redirect('/qr-scan')->with('success','Asistencia registrada');
