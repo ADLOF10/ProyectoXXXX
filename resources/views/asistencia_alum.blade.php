a30322a311147e86e7c1b6da5334abdfd3d7316b
marco12-coder <mar342c@gmail.com>
2025-01-05 23:24:22 -0600

    esti
    estilos we

--- resources/views/asistencia_alum.blade.php
+++ resources/views/asistencia_alum.blade.php
@@ -5,8 +5,8 @@
     <div class="section-header">
         <h1></h1>
         <div class="section-header-breadcrumb">
-          <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
-          <div class="breadcrumb-item">Profile</div>
+          <div class="breadcrumb-item active"><a href="#"></a></div>
+          <div class="breadcrumb-item"></div>
         </div>
       </div>
 
